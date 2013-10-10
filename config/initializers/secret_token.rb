d308d8a71957d81a18a88553050ed4f8be2747dd
Sean <sean@Seans-Mac.local>
2013-10-10 09:45:12 -0700

    Twitter Bootstrap Basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterBootstrap::Application.config.secret_key_base = '0d7b164071a6b0fd1866cbf5a5102420010404c017833daa2915afa45fe69f3184d2ef68d61f15e8c1e86aadda50d951346fbd800b0f1da7976e832a1b370974'
